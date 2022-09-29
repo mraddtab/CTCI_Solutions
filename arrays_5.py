ebe37253066f96f03f5049fe997fd74af36a017a
Ronald Chen <70644931+mraddtab@users.noreply.github.com>
2022-09-29 14:47:17 -0400

    Update arrays_5.py

--- arrays_5.py
+++ arrays_5.py
@@ -1,26 +1,50 @@
-#O(N) Given two strings check if they are 0 or 1 edits away from another
-# Insert Character (inplace), Remove, Insert
-# Algorithim
-#1. If the two strings are equal just return True
-#2. If the difference in lengths of strings is greater than 1, then more
-# than one insert/removal occured return False
-#3. If the lengths are the same, check for # of replaced characters, if
-# its greater than 1 return True, else return false.
-def one_away(str1, str2):
-  edit_counter = 0
-  if str1 == str2:
-    return True
+#Inserts c at index i and returns a new string
+
+#Determines if two strings are either one removal or one insert away.
+def oneInsRemAway(str1, str2):
+  shorter = list(min([str1, str2], key = len))
+  longer = list(max([str1,str2], key = len))
+  for i in range(0, len(shorter) + 1):
+    for c in longer:
+      shorter.insert(i, c)
+      if shorter == longer:
+        return True
+      del shorter[i]
+  return False
+
+def oneAway(str1, str2):
+  n_diff = 0 #records # differences in strings 
   if abs(len(str1) - len(str2)) > 1:
     return False
-  if abs(len(str1) - len(str2)) == 1:
-    edit_counter +=1
+  if str1 == str2:
+    return True
   if len(str1) == len(str2):
     for i in range(0, len(str1)):
       if str1[i] != str2[i]:
-        edit_counter +=1
-  return edit_counter <= 1
+        n_diff += 1
+    if n_diff > 1:
+      return False
+    else: 
+      return True
+  else:
+    return oneInsRemAway(str1,str2)
 
-print(one_away("pale", "ple"))
-print(one_away("pales", "pale"))
-print(one_away("pale", "bale"))
-print(one_away("pale", "bake"))
+# True
+print(" ***** True test Cases ******")
+print(oneAway("cat", "dat"))
+print(oneAway("abc", "abd"))
+print(oneAway("abc", "abcd"))
+print(oneAway("abcd", "acd"))
+print(oneAway("pale", "bale"))
+print(oneAway("", "a"))
+print(oneAway("", ""))
+print("")
+#False
+print(" ***** False test Cases ******")
+print(oneAway("abc", "def"))
+print(oneAway("abc", "abcdde"))
+print(oneAway("pal", "bale"))
+print(oneAway("paler", "railer"))
+print(oneAway("tetete", "hehe"))
+print(oneAway("", "aa"))
+print(oneAway("ccc", "bbb"))
